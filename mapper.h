47c7d084bf9cda73ac4f7fccd196ec5feb509752
jaggies <jaggiesjim@gmail.com>
2018-09-14 00:09:55 -0700

    Eliminate vector allocation in mapper.

--- mapper.h
+++ mapper.h
@@ -22,28 +22,24 @@ class Mapper: public Peripheral {
 
         // Reads first peripheral register at offset
         uint8_t read(int offset) override {
-            for (Peripheral* p : map(offset)) {
-                const int relativeAddr = offset - p->start();
-                uint8_t result = p->read(relativeAddr);
-                #ifdef DEBUG
-                std::cerr << "read " << p->name() << "[" << relativeAddr << "] = "
-                        << (int) result << std::endl;
-                #endif
-                return result;
+            for (Peripheral* p : _peripherals) {
+                if (offset >= p->start() && offset < p->end()) {
+                    const int relativeAddr = offset - p->start();
+                    uint8_t result = p->read(relativeAddr);
+                    return result; // always return the first one
+                }
             }
             return 0;
         }
 
-        // Writes all peripheral registers at offset
+        // Writes the first peripheral register found at offset
         void write(int offset, uint8_t value) override {
-            for (Peripheral* p : map(offset)) {
-                const int relativeAddr = offset - p->start();
-                #ifdef DEBUG
-                std::cerr << "write " << p->name() << "[" << relativeAddr << "] = "
-                        << (int) value << std::endl;
-                #endif
-                p->write(relativeAddr, value);
-                break; // Only write the first one we find
+            for (Peripheral* p : _peripherals) {
+                if (offset >= p->start() && offset < p->end()) {
+                    const int relativeAddr = offset - p->start();
+                    p->write(relativeAddr, value);
+                    break; // Only write the first one we find
+                }
             }
         }
 
@@ -61,15 +57,6 @@ class Mapper: public Peripheral {
 
     private:
         friend std::ostream& operator<<(std::ostream& os, const Mapper& mapper);
-        std::vector<Peripheral*> map(int addr) const {
-            std::vector<Peripheral*> result;
-            for (Peripheral* p : _peripherals) {
-                if (addr >= p->start() && addr < p->end()) {
-                    result.push_back(p);
-                }
-            }
-            return result;
-        }
         std::vector<Peripheral*> _peripherals;
 };
 
