8a47270e909aebaf22e7f07791f8e3faf1e759ba
jaggies <jaggiesjim@gmail.com>
2018-09-16 02:45:00 -0700

    Misc improvements to AedBus - add horizontal timing to video driver - update miscrd using hsync info - move doSerial() and doVideo() functions to compilation unit.

--- lib/aed/aedbus.cpp
+++ lib/aed/aedbus.cpp
@@ -14,6 +14,7 @@
 #include "aedregs.h"
 #include "ram.h"
 #include "generic.h"
+#include "io.h"
 
 #ifdef AED767
 #define SRAM_SIZE 2048
@@ -50,8 +51,14 @@ static const size_t VIDEO_MEM = 256 * 1024; // AED 512
 static const uint8_t SW1 = ~0x10; // negate since open is 0
 static const uint8_t SW2 = ~0x7d;
 
+// Video timing
+static const uint64_t LINE_TIME = SECS2USECS(1) / 15750;
+static const uint64_t FRAME_TIME = SECS2USECS(60) / 60;
+static const uint64_t LINE_SYNC_TIME = LINE_TIME / 20; // TODO
+
 AedBus::AedBus() : _mapper(0, CPU_MEM), _videoMemory(VIDEO_MEM, 0xff),
-            _pia0(0), _pia1(0), _pia2(0), _sio0(0), _sio1(0), _nextVideoSync(0) {
+            _pia0(0), _pia1(0), _pia2(0), _sio0(0), _sio1(0), _nextHsync(0), _nextVsync(0),
+            _hSync(0), _vSync(0) {
     // Open all ROM files and copy to ROM location in romBuffer
     std::vector<uint8_t> romBuffer;
     size_t offset = 0;
@@ -66,15 +73,15 @@ AedBus::AedBus() : _mapper(0, CPU_MEM), _videoMemory(VIDEO_MEM, 0xff),
     }
 
     // Add peripherals. Earlier peripherals are favored when addresses overlap.
-    _mapper.add(_pia0 = new M68B21(0x00, "PIA0", SW1));
-    _mapper.add(_pia1 = new M68B21(0x04, "PIA1"));
-    _mapper.add(_pia2 = new M68B21(0x08, "PIA2", SW2));
-    _mapper.add(_sio0 = new M68B50(0x0c, "SIO0"));
-    _mapper.add(_sio1 = new M68B50(0x0e, "SIO1"));
-    _mapper.add(new Generic(0x2a, 1,
-            [](int offset) { return random(); },
+    _mapper.add(_pia0 = new M68B21(pio0da, "PIA0", SW1));
+    _mapper.add(_pia1 = new M68B21(pio1da, "PIA1"));
+    _mapper.add(_pia2 = new M68B21(pio2da, "PIA2", SW2));
+    _mapper.add(_sio0 = new M68B50(sio0st, "SIO0"));
+    _mapper.add(_sio1 = new M68B50(sio1st, "SIO1"));
+    _mapper.add(new Generic(miscrd, 1,
+            [this](int offset) { return this->_hSync; },
             [](int offset, uint8_t value) { },
-            "aed2a"));
+            "miscrd"));
     _mapper.add(new AedRegs(0x00, 0x30, "aedregs"));
     _mapper.add(new Rom(0x10000 - romBuffer.size(), romBuffer));
     _mapper.add(new RamDebug(LED_BASE, SRAM_SIZE, "LED"));
@@ -90,4 +97,46 @@ AedBus::AedBus() : _mapper(0, CPU_MEM), _videoMemory(VIDEO_MEM, 0xff),
 
 AedBus::~AedBus() {
 }
+
+bool
+AedBus::doVideo() {
+   bool doIrq = false;
+   struct timeval tp = { 0 };
+   gettimeofday(&tp, NULL);
+   uint64_t now = tp.tv_sec * SECS2USECS(tp.tv_sec) + tp.tv_usec;
+   if (now > _nextVsync) {
+       _vSync = 1;
+       _nextVsync = now + FRAME_TIME; // 60Hz
+       doIrq = true;
+   } else if (now > _nextVsync - LINE_TIME) {
+       _vSync = 0;
+   }
+   if (now > _nextHsync) {
+       _hSync = 1;
+       _nextHsync = now + LINE_TIME; // 15kHz
+   } else if (now > _nextHsync - LINE_SYNC_TIME) {
+       _hSync = 0;
+   }
+   _vSync ? _pia1->assertLine(M68B21::CB1) : _pia1->deassertLine(M68B21::CB1);
+   return doIrq;
+}
+
+// Handles serial ports. Returns true if IRQ was generated
+bool
+AedBus::doSerial() {
+   uint8_t byte;
+   if (_sio0->transmit(&byte)) {
+       std::cout << "SIO0: " << (int) byte << std::endl;
+   }
+
+   if (_sio1->transmit(&byte)) {
+       std::cout << "SIO1: " << (int) byte << std::endl;
+   }
+
+   if (!_serialFifo.empty() && _sio0->receive(_serialFifo.front())) {
+       _serialFifo.pop();
+   }
+   return _sio0->irqAsserted() || _sio1->irqAsserted();
+}
+
 
--- lib/aed/aedbus.h
+++ lib/aed/aedbus.h
@@ -27,38 +27,15 @@ class AedBus : public BUS {
         uint8_t read(int addr) override { return _mapper.read(addr); }
         void write(int addr, unsigned char value) override { _mapper.write(addr, value); }
 
-        // TODO: Use SIGALARM instead of calculating this every time!
-        bool doVideo() {
-            struct timeval tp = { 0 };
-            gettimeofday(&tp, NULL);
-            uint64_t now = tp.tv_sec * SECS2USECS(tp.tv_sec) + tp.tv_usec;
-            if (now > _nextVideoSync) {
-                _pia1->assertLine(M68B21::CB1);
-                _nextVideoSync = now + SECS2USECS(60) / 60; // 60Hz
-                return true;
-            } else {
-                _pia1->deassertLine(M68B21::CB1);
-                return false;
-            }
-        }
+        // Handle video-related timing and return 'true' if IRQ needs to happen
+        // TODO: Handle this better. Maybe use SIGUSR interrupt.
+        bool doVideo();
 
-        // Handles serial ports. Returns true if IRQ was generated
-        bool doSerial() {
-            uint8_t byte;
-            if (_sio0->transmit(&byte)) {
-                std::cout << "SIO0: " << (int) byte << std::endl;
-            }
+        // Looks for data from serial ports. Returns true if IRQ needs to happen.
+        // TODO: Have this automatically invoke serial callback when requested by CPU write
+        bool doSerial();
 
-            if (_sio1->transmit(&byte)) {
-                std::cout << "SIO1: " << (int) byte << std::endl;
-            }
-
-            if (!_serialFifo.empty() && _sio0->receive(_serialFifo.front())) {
-                _serialFifo.pop();
-            }
-            return _sio0->irqAsserted() || _sio1->irqAsserted();
-        }
-
+        // Copies string to FIFO for handling in doSerial()
         void send(const std::string& string) {
             for (char c : string) {
                 _serialFifo.push(c);
@@ -73,7 +50,10 @@ class AedBus : public BUS {
         M68B21 * _pia2;
         M68B50 * _sio0;
         M68B50 * _sio1;
-        uint64_t _nextVideoSync;
+        uint64_t _nextVsync;
+        uint64_t _nextHsync;
+        uint8_t _vSync;
+        uint8_t _hSync;
         std::queue<uint8_t> _serialFifo;
 };
 
