cbc5062dd915963b35f06f727d210dcdcad0d5ac
jaggies <jaggiesjim@gmail.com>
2018-09-17 01:56:54 -0700

    Allow override of ROM with additional flag to facilitate testing

--- lib/core/rom.h
+++ lib/core/rom.h
@@ -14,15 +14,22 @@
 
 class Rom: public Ram {
     public:
-        Rom(int start, const std::vector<uint8_t>& init) : Ram(start, init.size(), "ROM") {
+        Rom(int start, const std::vector<uint8_t>& init, bool allowWrites = false)
+                : Ram(start, init.size(), "ROM"), _allowWrites(allowWrites) {
             assert(init.size() == _storage.size());
             std::copy(init.begin(), init.end(), _storage.begin());
         }
         virtual ~Rom() = default;
 
         void write(int offset, uint8_t value) override {
-            std::cerr << "Ooops, tried to write to ROM: offset = " << offset << std::endl;
+            if (_allowWrites) {
+                Ram::write(offset, value);
+            } else {
+                std::cerr << "Ooops, tried to write to ROM: offset = " << offset << std::endl;
+            }
         }
+    private:
+        bool _allowWrites;
 };
 
 #endif /* ROM_H_ */
