0b6ae7d66c45a8fd613b8183452611aa2edca365
jaggies <jaggiesjim@gmail.com>
2018-10-16 01:29:19 -0700

    Supersample fractal since we have a lot of CPU cycles to burn on the host

--- apps/demos/mandl.cpp
+++ apps/demos/mandl.cpp
@@ -17,6 +17,7 @@ static AedSequence seq;
 const float centerX = -0.722;
 const float centerY = 0.244095;
 const float width = 0.00019;
+const int SAMPLES = 256; // number of samples per pixel
 
 void mandel(double xmin, double xmax, double ymin, double ymax) {
     float cr_delta = (xmax - xmin)/(xres-1);
@@ -27,18 +28,24 @@ void mandel(double xmin, double xmax, double ymin, double ymax) {
     for (int j = 0; j < yres; j++, ci += ci_delta) {
         float cr = xmin;
         for (int i = 0; i < xres; i++, cr += cr_delta) {
-            float zr = 0;
-            float zi = 0;
-            int count = 0;
-            do {
-                // z = z^2 + c
-                // (zr + zi)*(zr + zi) = zr*zr + 2*zr*zi - zi*zi
-                float tr = zr * zr - zi * zi;
-                float ti = 2.0f * zr * zi;
-                zr = tr + cr;
-                zi = ti + ci;
-            } while ((count++ < maxCount) && (zr*zr + zi*zi) < 4.0f);
-            buffer[i] = count % 256;
+            int sum = 0;
+            for (int k = 0; k < SAMPLES; k++) {
+                float zr = 0;
+                float zi = 0;
+                int count = 0;
+                float jcr = cr + cr_delta * drand48();
+                float jci = ci + ci_delta * drand48();
+                do {
+                    // z = z^2 + c
+                    // (zr + zi)*(zr + zi) = zr*zr + 2*zr*zi - zi*zi
+                    float tr = zr * zr - zi * zi;
+                    float ti = 2.0f * zr * zi;
+                    zr = tr + jcr;
+                    zi = ti + jci;
+                } while ((count++ < maxCount) && (zr*zr + zi*zi) < 4.0f);
+                sum += count;
+            }
+            buffer[i] = (sum / SAMPLES) % 256;
         }
         seq.mov(0, j).write_horizontal_runs(buffer, xres).send();
     }
